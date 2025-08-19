b517f10fdb085fd4c7adc06fb0db22c5125e6048
Nilesh Agrahari <nagrahari539@gmail.com>
2025-08-19 22:24:54 +0530

    setup

---
+++ Park_EYE/Park_EYE/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Park_EYE project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Park_EYE.settings')
+
+application = get_asgi_application()
